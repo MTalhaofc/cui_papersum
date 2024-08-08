675aa2578f1f58b58769bbff2dbe9b1cbc54a208
MTalhaofc <talha0750@gmail.com>
2024-08-08 22:32:12 +0500

    For Vite server 5 try

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-
-
-
     build: {
         outDir: 'public/build', // Ensure this matches Laravel's expectations
+        manifest: true, // Generate a manifest file for Laravel to use
     },
-    server:{
-        host: '0.0.0.0',
+    server: {
+        host: '0.0.0.0', // Ensure this is accessible from the Heroku environment
         port: 5173, // Make sure the port is available
-        
-   
-fs:{
-    strict:false,
-},
-}
-,
+        fs: {
+            strict: false,
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
