d72bbc741733a6ba97958d6045d6c7ac910be6ea
MTalhaofc <talha0750@gmail.com>
2024-08-08 22:59:23 +0500

    Vite Server

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     build: {
-        outDir: 'public/build', // Ensure this matches Laravel's expectations
-        manifest: true, // Generate manifest file
     },
     server: {
         host: process.env.VITE_IP_ADDR || '0.0.0.0', // Use environment variable or default to '0.0.0.0'
