ec78468bb1a22dae5e3fa3fb9436ff1cbe59a79f
MTalhaofc <talha0750@gmail.com>
2024-08-09 00:13:20 +0500

    Tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="icon" type="svg" class="h-1/2" href="{{ asset('assets/Browser_logo_cuipapersum.svg') }}">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 
     <script src="https://kit.fontawesome.com/e28e3997b3.js" crossorigin="anonymous"></script>
 
