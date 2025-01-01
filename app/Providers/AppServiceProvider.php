ee7794f9bd0a3bb3432cc289e5f86c6f76f9047f
MTalhaofc <talha0750@gmail.com>
2025-01-01 20:16:38 +0500

    ASP Updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
