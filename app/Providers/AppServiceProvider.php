9757d2eb83d7b58f3507c44ef2030c51c29d7382
MTalhaofc <talha0750@gmail.com>
2025-01-01 20:20:14 +0500

    ASP REVERSED

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
+        
     }
 }
