4943035e55ebac61eca3982cb5c8bcf958a7ddb5
MTalhaofc <talha0750@gmail.com>
2025-01-03 01:43:54 +0500

    Multiple Request

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      /* Use https links instead http links */
-      if (app()->environment('remote')) {
-        URL::forceScheme('https');
-    }
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
