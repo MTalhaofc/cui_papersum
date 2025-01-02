7b431642e2b146e2dfaf4899cc096f7c1c556890
MTalhaofc <talha0750@gmail.com>
2025-01-03 01:37:29 +0500

    Multiple Request

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Request;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -24,26 +25,11 @@ class AppServiceProvider extends ServiceProvider
 
      */
     public function boot()
-{
-    // Trust all proxies (required for Heroku)
-    Request::setTrustedProxies(
-        ['*'], // Trust all proxies
-        Request::HEADER_X_FORWARDED_ALL // Trust X-Forwarded headers
-    );
-
-    // Force HTTPS if the request is not secure
-    if ($this->isHttpRequest()) {
-        URL::forceScheme('https');
+    {
+      /* Use https links instead http links */
+      if (Request::server('HTTP_X_FORWARDED_PROTO') == 'https')
+      {
+         URL::forceScheme('https');
+      }
     }
-}
-
-/**
- * Check if the current request is HTTP (and not HTTPS).
- *
- * @return bool
- */
-protected function isHttpRequest()
-{
-    return request()->header('X-Forwarded-Proto') !== 'https';
-}
 }
